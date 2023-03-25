903639eeaaa3579254d65841ed42a7f25c741e72
chia313339 <chia313339@gmail.com>
2023-03-25 21:26:15 +0800

    20230325

--- app.js
+++ app.js
@@ -1,53 +1,300 @@
 const data = [
     {
-        type: "影音平台",
+        type: ["直播","其他"],
         name: "Youtube",
         url: "https://www.youtube.com",
-        img: "https://www.youtube.com/img/desktop/yt_1200.png",
+        img: "image/web/44.jpg",
     },
     {
-        type: "影音平台",
+        type: ["電影","電視劇","綜藝","動漫"],
         name: "Netflex",
         url: "https://www.netflix.com/tw/",
-        img: "https://www.citypng.com/public/uploads/preview/-11594687246vzsjesy7bd.png",
+        img: "image/web/003.png",
     },
     {
-        type: "電視直播",
+        type: ["直播",],
         name: "四季線上",
         url: "https://www.4gtv.tv/channel/4gtv-4gtv003?set=4&ch=1",
-        img: "https://m.4gtv.tv/image/4gTV.png",
+        img: "image/web/50.jpg",
     },
     {
-        type: "電視直播",
+        type: ["直播",],
         name: "TV線上看電視",
         url: "https://tv.wfuapp.com/2019/12/pts-live.html",
-        img: "image/web/001.png",
+        img: "image/web/51.jpg",
     },
     {
-        type: "電影戲劇",
+        type: ["電影","電視劇","綜藝","動漫","其他",],
         name: "小鴨看片",
         url: "https://777tv.app/vod/show/area/%E8%87%BA%E7%81%A3/id/1.html",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpq8czMUFJcsOu4zqff4jhyF0qHejmiZqPLQ&usqp=CAU.JPG",
+        img: "image/web/1.jpg",
     },
     {
-        type: "電影戲劇",
+        type: ["電影","電視劇","綜藝","動漫",],
         name: "DramasQ",
         url: "https://dramasq.cc/",
-        img: "image/web/002.png",
+        img: "image/web/33.jpg",
     },
     {
-        type: "其他",
-        name: "google",
+        type: ["直播",],
+        name: "HiNet 免費影音",
+        url: "https://www.hinet.net/tv/",
+        img: "image/web/48.jpg",
+    },
+    {
+        type: ["直播",],
+        name: "Touch TTV 台視",
+        url: "https://www.touchttv.com/touchttv/default.aspx",
+        img: "image/web/49.jpg",
+    },
+    {
+        type: ["直播",],
+        name: "皮克直播",
+        url: "https://piko.live/",
+        img: "image/web/52.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "LINE TV",
+        url: "https://www.linetv.tw/",
+        img: "image/web/005.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "愛奇藝",
+        url: "https://www.iq.com/?lang=zh_tw",
+        img: "image/web/006.png",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "Disney Plus",
+        url: "https://www.disneyplus.com/zh-tw",
+        img: "image/web/007.png",
+    },
+        {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "Gimy 劇迷",
+        url: "https://pttplay.cc/",
+        img: "image/web/2.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫","其他",],
+        name: "Gimy TV",
+        url: "https://gimys.tv/",
+        img: "image/web/3.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "劇迷TV",
+        url: "https://jumi.tv/",
+        img: "image/web/4.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫","其他",],
+        name: "泥視頻",
+        url: "https://www.nivod4.tv/index.html",
+        img: "image/web/5.jpg",
+    },
+    {
+        type: ["電影","電視劇","動漫","其他",],
+        name: "Movieffm",
+        url: "https://www.movieffm.net",
+        img: "image/web/6.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫","其他",],
+        name: "MoMoVOD",
+        url: "https://momovod.app/",
+        img: "image/web/7.jpg",
+    },
+    {
+        type: ["直播",],
+        name: "Twitch",
+        url: "https://www.twitch.tv/",
+        img: "image/web/004.png",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "Balatutu",
+        url: "https://balatutu.me/",
+        img: "image/web/8.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "片庫",
+        url: "https://www.pkmp4.com/",
+        img: "image/web/9.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫","其他",],
+        name: "555电影",
+        url: "https://www.o8tv.com/",
+        img: "image/web/10.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫","其他",],
+        name: "94i影城",
+        url: "https://94itv.app",
+        img: "image/web/11.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "inmi 映迷",
+        url: "https://www.inmi.app",
+        img: "image/web/12.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "LoveTvShow",
+        url: "https://lovetv.show",
+        img: "image/web/13.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "楓林網",
+        url: "https://8maple.tv/",
+        img: "image/web/17.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "黑料電影",
+        url: "https://dy12.fun/",
+        img: "image/web/39.jpg",
+    },
+    {
+        type: ["動漫",],
+        name: "樱花动漫",
+        url: "www.yinghuacd.com",
+        img: "image/web/34.jpg",
+    },
+    {
+        type: ["動漫",],
+        name: "在线动画",
+        url: "https://www.agemys.net/",
+        img: "image/web/35.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "D1DM 第一動漫",
+        url: "https://d1-dm.online/",
+        img: "image/web/36.jpg",
+    },
+    {
+        type: ["其他",],
+        name: "Google",
         url: "https://google.com/",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK5q0FP74VV9wbfwP378_7kj7iDomHuKrxkXsxDdUT28V9dlVMNUe-EMzaLwaFhneeuZI&usqp=CAU",
-        },
+        img: "image/web/42.jpg"
+    },
+    {
+        type: ["其他",],
+        name: "Yahoo",
+        url: "https://tw.yahoo.com/",
+        img: "image/web/43.jpg",
+    },
+    {
+        type: ["其他",],
+        name: "Facebook",
+        url: "https://www.facebook.com/",
+        img: "image/web/45.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "低端影视",
+        url: "https://ddys.art/",
+        img: "image/web/14.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "94580",
+        url: "https://94580.net/",
+        img: "image/web/16.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "努努影院",
+        url: "https://www.nunuyy5.org/",
+        img: "image/web/18.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "今日影視",
+        url: "https://jinri.live/",
+        img: "image/web/19.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "Facebook",
+        url: "",
+        img: "image/web/45.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "99kubo酷播",
+        url: "https://123kubo.net/",
+        img: "image/web/20.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫","其他",],
+        name: "欧乐影院",
+        url: "https://www.olevod.com",
+        img: "image/web/21.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫","其他",],
+        name: "伊莉影音",
+        url: "https://eynytv.com",
+        img: "image/web/22.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "小白影音",
+        url: "https://www.maxbaytw.com/",
+        img: "image/web/24.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "蛋蛋赞影院",
+        url: "https://www.dandanzan.cc",
+        img: "image/web/25.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "53tv",
+        url: "https://53tv.net/",
+        img: "image/web/40.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "看韩剧",
+        url: "https://www.kangii.com/",
+        img: "image/web/26.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "狗狗影音",
+        url: "https://dogevod.com/",
+        img: "image/web/41.jpg",
+    },
+    {
+        type: ["電視劇","綜藝","動漫",],
+        name: "韓劇網",
+        url: "https://kortw.com/",
+        img: "image/web/27.jpg",
+    },
+    {
+        type: ["電影","電視劇","綜藝","動漫",],
+        name: "WeTV",
+        url: "https://wetv.vip/zh-tw",
+        img: "image/web/30.jpg",
+    },
+    
 
 ];
 
 const app = Vue.createApp({
     data(){
         return{
-            filters: ['All', '影音平台', '電視直播', '電影戲劇', '其他'],
+            filters: ['All', '電影', '電視劇', '綜藝', '動漫', '直播', '其他'],
             selectedFilter: 'All',
             items: data,
         }
@@ -55,11 +302,15 @@ const app = Vue.createApp({
     computed: {
         filteredItems() {
             if (this.selectedFilter === 'All') {
-            return this.items.map((item) => item);
+                return this.items.map((item) => item);
             }
-            return this.items
-            .filter((item) => item.type === this.selectedFilter)
-            .map((item) => item);
+            return this.items.filter((item) => {
+                if (Array.isArray(item.type)) {
+                return item.type.includes(this.selectedFilter);
+                } else {
+                return item.type === this.selectedFilter;
+                }
+            });
         },
         },
         methods: {
